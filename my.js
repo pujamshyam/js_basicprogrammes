7fbdbad15149fbb7c779c5bb8131370a88d344e7
Pujam.Shyam <81807241+pujamshyam@users.noreply.github.com>
2021-08-04 20:19:32 +0530

    Add files via upload

---
+++ my.js
@@ -0,0 +1,13 @@
+console.log("reverse of a array...")
+let arr=['1','2','3','4','5'];
+for(let i=arr.length-1; i>=0; i--){
+    console.log(arr[i]);
+}
+console.log("elements divisible by 5 from 1 to 50 are...")
+ let j=1;
+while(j<50){
+    if(j%5 == 0){
+        console.log(j);
+    }
+    j++;
+}
